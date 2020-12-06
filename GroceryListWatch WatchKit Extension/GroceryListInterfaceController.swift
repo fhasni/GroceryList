55f2629deabbd890c1b23d3d350380a2ef745611
fhasni <hasni.faissal@gmail.com>
2020-12-06 17:46:48 +0100

    first commit

---
+++ GroceryListWatch WatchKit Extension/GroceryListInterfaceController.swift
@@ -0,0 +1,76 @@
+//
+//  HomeInterfaceController.swift
+//  GroceryListWatch WatchKit Extension
+//
+//  Created by fhasni on 12/5/20.
+//
+
+import WatchKit
+import Foundation
+
+
+class GroceryListInterfaceController: WKInterfaceController {
+
+    @IBOutlet weak var addItemBtn: WKInterfaceButton!
+    
+    @IBOutlet weak var groceryTable: WKInterfaceTable!
+    
+    private var items : [String] = []
+    
+    private var initialSuggestions : [String] = ["Bread", "Milk", "Eggs", "Cheese"]
+    
+    override func awake(withContext context: Any?) {
+        // Configure interface objects here.
+        loadTable()
+    }
+    
+    @IBAction func addItemTapped() {
+        presentTextInputController(withSuggestions: suggestions(), allowedInputMode: .plain) { [weak self] result in
+            guard let result = result else { return }
+            guard let newItem = result[0] as? String else { return }
+            OperationQueue.main.addOperation {
+                self?.addItem(newItem)
+            }
+        }
+        
+    }
+    
+    override func table(_ table: WKInterfaceTable, didSelectRowAt rowIndex: Int) {
+        // this methode is called when a table row is selected
+        print("DEBUG: Item selected: \(items[rowIndex])")
+        
+        presentAlert(withTitle: nil, message: "Are you sure you want to delete \"\(items[rowIndex])\"", preferredStyle: .actionSheet, actions: [WKAlertAction(title: "Confirm", style: .destructive, handler: { [weak self] in
+            self?.deleteItem(at: rowIndex)
+            print("DEBUG: delete item at index: \(rowIndex)")
+        })])
+    }
+    
+    
+    func loadTable() {
+        groceryTable.setNumberOfRows(items.count, withRowType: GroceryRow.identifier)
+        for (index, item) in items.enumerated() {
+            let groceryRow = groceryTable.rowController(at: index) as? GroceryRow
+            groceryRow?.label.setText(item)
+        }
+    }
+    
+    func addItem(_ item: String) {
+        print("DEBUG: adding item: \(item)")
+
+        items.append(item)
+        loadTable()
+    }
+    
+    func deleteItem(at index: Int) {
+        print("DEBUG: deleting item at index: \(index)")
+        items.remove(at: index)
+        loadTable()
+    }
+    
+    func suggestions() -> [String] {
+        return initialSuggestions.filter {
+            !items.contains($0)
+        }
+    }
+    
+}
---
+++ GroceryListWatch WatchKit Extension/GroceryRow.swift
@@ -0,0 +1,15 @@
+//
+//  GroceryRow.swift
+//  GroceryListWatch WatchKit Extension
+//
+//  Created by fhasni on 12/6/20.
+//
+
+import Foundation
+import WatchKit
+
+class GroceryRow: NSObject {
+    static let identifier = "GroceryRow"
+    
+    @IBOutlet weak var label: WKInterfaceLabel!
+}
